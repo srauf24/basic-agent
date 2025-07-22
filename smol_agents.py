953012db4bea06b47212cbc6f7d5be8c9bd2da8e
Samee Rauf <samee.rauf@pmg.com>
2025-07-21 19:19:31 -0500

    added dependency list to uv.lock

---
+++ smol_agents.py
@@ -0,0 +1,7 @@
+#https://huggingface.co/blog/smolagents
+
+from smolagents import CodeAgent, DuckDuckGoSearchTool, HfApiModel
+
+agent = CodeAgent(tools=[DuckDuckGoSearchTool()], model=HfApiModel())
+
+agent.run("How many seconds would it take for a leopard at full speed to run through Pont des Arts?")
